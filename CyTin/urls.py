af222e338013b813d06eb82ea27af0b3a224b62b
Chandan Prasad <cp564738@gmail.com>
2018-03-10 11:38:22 +0530

    Static Files working

--- CyTin/urls.py
+++ CyTin/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from CyTin_View.views import home
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+	url(r'^$', home),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
