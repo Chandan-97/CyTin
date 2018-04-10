19ce028ac18d623c3f25bca7cf92cbf0aed2f701
Chandan Prasad <cp564738@gmail.com>
2018-04-10 08:29:32 +0530

    Fix Pluralization into DB Admin

--- CyTin_View/models.py
+++ CyTin_View/models.py
@@ -34,6 +34,9 @@ class News(models.Model):
 		return self.title
 
 class Requestnew(models.Model):
+	class Meta:
+		verbose_name_plural = "RequestNew"
+		
 	software = models.CharField(max_length=30, null=False)
 	version  = models.CharField(max_length=20, null=True, default="N/A")
 	comment  = models.CharField(max_length=600, null=True, default="No Comments")
